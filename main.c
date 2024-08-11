91bdad81448a7f4a7fd24580a1a3bfe1b0020df6
Meow <mrlinusmeow@skiff.com>
2024-08-11 14:18:44 +0200

    fixed moving logic; fixed bug with no updating of state; updated comments

--- main.c
+++ main.c
@@ -1,28 +1,29 @@
 #include <stdlib.h>
 #include <assert.h>
+#include <stdint.h>
 
 int main(){
-	volatile short const maze = 0x1554; // maze: 0001 0101 0101 0100
-	short hans = 0x0008; // position of hans: 0000 0000 0000 1000
-	short prev = hans; // remember hans's state
-	short state = ((maze ^ hans) == maze) ? -1 : maze ^ hans; // checks if hans and maze will collide
-	assert(state != -1); // then it will assert()
-	volatile char Y = 1; // up
-	volatile char X = 1; // right
-	volatile char moveDir = 0; // 0: move direction Y , 1: move direction X , else do nothing
+	uint16_t const maze = 0x1554; // maze: 0001 0101 0101 0100
+	static uint16_t hans = 0x0008; // position of hans: 0000 0000 0000 1000
+	uint16_t prev = hans; // remember hans's state
+	static uint16_t state = maze; // checks if hans and maze will collide
+	volatile uint16_t Y = 1; // 0: down , 1: up
+	volatile uint16_t X = 1; // 0: left , 1: right
+	volatile uint8_t moveDir = 2; // 0: move direction Y , 1: move direction X , else do nothing
 	do{
 		asm("nop"); // does nothing
-		state = ((state - maze) == hans) ? state : state^prev; // removes previous hans if present in state
-		prev = hans; // previous hans is now hans
+		state += hans; // update hans' postion
+		state = ((state - maze) == hans) ? state : state^prev; // removes previous hans if it's present in state
+		prev = hans; // remember hans's state
 		if(moveDir == 0){ // vertical direction
-			Y = Y ? (hans <<= 4) : (hans >>= 4); // go up else go down
-			assert((Y & maze) != 0); // assert when hans is colliding with wall
-			hans = Y; // hans is moved
+			hans = Y ? (hans << 4) : (hans >> 4); // move up or down
+			assert(hans != 0); // assert() when hans is out of bounds
+			assert((maze & hans) != maze); // assert() when hans collides with maze wall
 		}else if(moveDir == 1){ // horizontal direction
-			X = X ? (hans >>= 1) : (hans <<= 1); // go right else go left
-			assert((X & maze) != 0); // assert when hans is colliding with wall
-			hans = X; // hans is moved
+			hans = X ? (hans >> 1) : (hans << 1); // move right or left
+			assert(hans != 0); // assert() when hans is out of bounds
+			assert((maze & hans) != maze); // assert() when hans collides with maze wall
 		}else continue; // do not move
-	}while(state != 0x1555);
-	return 0;
+	}while(state != 0x1555); // check if hans' completed the maze
+	return 0; // success
 }
