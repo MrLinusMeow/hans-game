bb8c2362ef003d8d81da8f66ffc15b5030ff8f86
Meow <mrlinusmeow@skiff.com>
2024-08-11 12:50:58 +0200

    changed variable names; fixed up conditions and movement; replaced abort() with assert(); added nop instruction; added comments

--- main.c
+++ main.c
@@ -2,23 +2,27 @@
 #include <assert.h>
 
 int main(){
-	short const maze = 0x1554; // 0001 0101 0101 0100
-	short hans = 0x0008; // 0000 0000 0000 1000
-	short temp = hans;
-	short state = ((maze ^ hans) == maze) ? -1 : maze ^ hans;
+	volatile short const maze = 0x1554; // maze: 0001 0101 0101 0100
+	short hans = 0x0008; // position of hans: 0000 0000 0000 1000
+	short prev = hans; // remember hans's state
+	short state = ((maze ^ hans) == maze) ? -1 : maze ^ hans; // checks if hans and maze will collide
+	assert(state != -1); // then it will assert()
+	volatile char Y = 1; // up
+	volatile char X = 1; // right
+	volatile char moveDir = 0; // 0: move direction Y , 1: move direction X , else do nothing
 	do{
-		volatile char y = 1;
-		volatile char x = 1;
-		hans = (temp == hans) ? hans : 0;
-		if(hans == 0) abort();
-		state = ((state - maze) == hans) ? state : state^temp;
-		temp = hans;
-		y = y ? (hans <<= 4) : (hans >>= 4);
-		if( ((y & maze) == 0) ) abort();
-		else { temp = hans; hans = y; continue; }
-		x = x ? (hans >>= 1) : (hans <<= 1);
-		if( ((x & maze) == 0) ) abort();
-		else { temp = hans; hans = x; continue; }
+		asm("nop"); // does nothing
+		state = ((state - maze) == hans) ? state : state^prev; // removes previous hans if present in state
+		prev = hans; // previous hans is now hans
+		if(moveDir == 0){ // vertical direction
+			Y = Y ? (hans <<= 4) : (hans >>= 4); // go up else go down
+			assert((Y & maze) != 0); // assert when hans is colliding with wall
+			hans = Y; // hans is moved
+		}else if(moveDir == 1){ // horizontal direction
+			X = X ? (hans >>= 1) : (hans <<= 1); // go right else go left
+			assert((X & maze) != 0); // assert when hans is colliding with wall
+			hans = X; // hans is moved
+		}else continue; // do not move
 	}while(state != 0x1555);
 	return 0;
 }
